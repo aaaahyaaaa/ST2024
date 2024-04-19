ca221518860cb490fc015b266510ece63286de7c
aaaahyaaaa <2920346580@qq.com>
2024-04-19 19:35:51 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,7 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -16,10 +18,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+        
+        
         
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -45,7 +57,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 待办事项表格中显示了"1: Buy flowers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
         
         
         # 页面中又显示了一个文本框，可以输入其她待办事项
@@ -53,11 +65,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Send a gift to Chenzhuhao')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Send a gift to Chenzhuhao')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Send a gift to Chenzhuhao')
 
         # 韩烨想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
